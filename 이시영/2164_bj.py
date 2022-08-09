75956a7eeb551993f443d2635d0e59b9546803dc
Lee-Siyoung <tldud4861@gmail.com>
2022-08-09 22:38:06 +0900

    Create 2164_bj.py

---
+++ 이시영/2164_bj.py
@@ -0,0 +1,9 @@
+from sys import stdin
+from collections import deque
+n=int(stdin.readline())
+q=deque([i for i in range(1,n+1)])
+
+while len(q)!=1:
+    q.popleft()
+    q.append(q.popleft())
+print(q[0])

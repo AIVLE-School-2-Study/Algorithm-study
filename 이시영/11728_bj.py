70d76ee4b057bfa6692f371bb470ca029c680a05
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2022-09-23 23:32:06 +0900

    Revert "9.22 ~ 9.23"

--- 이시영/11728_bj.py
+++
@@ -1,7 +0,0 @@
-from sys import stdin
-
-n, m = map(int,stdin.readline().split())
-a = list(map(int,stdin.readline().split()))
-b = list(map(int,stdin.readline().split()))
-
-print(" ".join(map(str,sorted(a+b))))
